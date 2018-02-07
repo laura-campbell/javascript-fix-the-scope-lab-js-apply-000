6435702fb6d0e5fb0cb7a519478467e76613d26a
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-07 08:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function yourAnimal() {
   return youranimal
 }
 
+function add2(n) {
+  return n + 2
 
+  // Feel free to move things around!
+  const two = 2
+}
 
 var funkyFunction = function() {
   return function() {
